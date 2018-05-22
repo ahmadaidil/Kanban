2442fde5b707239222a7c1283f87678da1cac8dd
Ahmad Aidil <aidil@kofera.com>
2018-05-22 15:39:57 +0700

    added style

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,13 @@
     <router-view/>
   </div>
 </template>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
