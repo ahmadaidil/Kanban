dede5239454614bd6c6a3dadaeba4163758968a4
Ahmad Aidil <aidil@kofera.com>
2018-05-22 14:27:41 +0700

    My kanban version (without vuefire)

---
+++ src/components/Board.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-sm-4">
+    <div :class="`panel panel-${className}`">
+      <div class="panel-heading">{{ name }}</div>
+      <div class="panel-body">
+        <draggable class="drag" v-model="data" :options="{group:'kanban'}" @start="drag=true" @end="drag=false">
+          <div :class="`alert alert-${className} pointer-cursor`" v-for="({ note }, index ) in data" :key="index">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            {{note}}
+            </div>
+        </draggable>
+      </div>
+      <div class="panel-footer"><a data-toggle="modal" data-target="#myModal" class="btn btn-secondary" @click='changeType(type)'>Add Sticky Note</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+
+export default {
+  components: {
+    draggable
+  },
+  props: {
+    data: Array,
+    name: String,
+    type: String,
+    className: String,
+    changeType: Function
+  }
+}
+</script>
+
+<style scoped>
+  .drag {
+    min-height: 3em;
+  }
+  .pointer-cursor {
+    cursor: pointer;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    <h1>YOUR KANBAN</h1><br>
+      <div class="container">
+        <board v-bind="{
+          data: todo,
+          name: 'TO DO',
+          type: 'todo',
+          className: 'danger',
+          changeType
+        }"></board>
+        <board v-bind="{
+          data: ongoing,
+          name: 'ON GOING',
+          type: 'ongoing',
+          className: 'info',
+          changeType
+        }"></board>
+        <board v-bind="{
+          data: done,
+          name: 'DONE',
+          type: 'done',
+          className: 'success',
+          changeType
+        }"></board>
+      </div>
+      <modal v-bind="{ note, type, onChangeNote, addNote }"></modal>
+  </div>
+</template>
+
+<script>
+import Board from '../components/Board'
+import Modal from '../components/Modal'
+
+export default {
+  name: 'home',
+  components: {
+    Board,
+    Modal
+  },
+  data: () => ({
+    todo: [],
+    ongoing: [],
+    done: [],
+    note: '',
+    type: ''
+  }),
+  methods: {
+    addNote (type) {
+      this[type].push({
+        note: this.note
+      })
+      this.note = ''
+      this.type = ''
+    },
+    changeType (newType) {
+      this.type = newType
+    },
+    onChangeNote (newNote) {
+      this.note = newNote
+    }
+  }
+}
+</script>
